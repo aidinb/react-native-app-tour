4bf3edc247da4d631787dadf43c60b5f5ba2698f
aidin <aidin.b2009@gmaill.com>
2022-10-25 13:18:58 +0300

    init

---
+++ ios/RNAppTour.h
@@ -0,0 +1,7 @@
+#import <React/RCTUIManager.h>
+
+@interface MutableOrderedDictionary<__covariant KeyType, __covariant ObjectType> : NSDictionary<KeyType, ObjectType>
+@end
+
+@interface RNAppTour : NSObject
+@end
